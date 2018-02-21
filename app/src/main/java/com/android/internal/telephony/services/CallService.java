4b3916a3ea3356ae2be6b84cebd63d5f5726393e
Abdelrahman1309 <34864293+Abdelrahman1309@users.noreply.github.com>
2018-02-21 18:03:59 +0200

    Best Stable Version till now

--- app/src/main/java/com/android/internal/telephony/services/CallService.java
+++ app/src/main/java/com/android/internal/telephony/services/CallService.java
@@ -12,17 +12,13 @@ import android.media.AudioTrack;
 import android.media.MediaRecorder;
 import android.os.IBinder;
 import android.util.Log;
-
 import com.android.internal.telephony.utils.Constants;
-
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-
-import io.reactivex.Observable;
 import io.reactivex.disposables.CompositeDisposable;
 
 //Todo (1) register broadcast receiver to receive commands from other app parts - done
@@ -37,7 +33,7 @@ public class CallService extends Service {
     private static final int SAMPLE_SIZE = 2; // Bytes
     private static final int minBufSize = SAMPLE_INTERVAL * SAMPLE_INTERVAL * SAMPLE_SIZE * 2; //Bytes
     private static String mTag;
-    private CompositeDisposable mDisposable = new CompositeDisposable();
+    private short x = 0,y = 0;
 
     @Override
     public void onCreate() {
@@ -51,61 +47,37 @@ public class CallService extends Service {
         registerReceiver(mCallServiceReceiver, callServiceIntentFilter);
     }
 
-    private void startCallServer() {
+    private void startSpeaker() {
         Thread thread = new Thread(() -> {
+            //Instance for speaker
+            AudioTrack atrack = new AudioTrack(AudioManager.STREAM_VOICE_CALL, Constants.Calling.SAMPLING_RATE,
+                    AudioFormat.CHANNEL_OUT_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT, minBufSize, AudioTrack.MODE_STREAM);
+            //start speaker
+            atrack.play();
             try {
+                x = 1;
                 Log.i(mTag, "Call Server started");
                 //Servers to receive from client
                 DatagramSocket serverSocket = new DatagramSocket(Constants.Calling.CALLING_SERVER_PORT);
-                serverSocket.setReuseAddress(true);
-                //buffer to receive from microphone
-                byte[] buffer = new byte[minBufSize];
-                //Instance for microphone
-                //AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,Constants.Calling.SAMPLING_RATE,channelConfig,audioFormat,minBufSize*10);
-
-                AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, Constants.Calling.SAMPLING_RATE,
-                        AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,
-                        AudioRecord.getMinBufferSize(Constants.Calling.SAMPLING_RATE,
-                                AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT) * 10);
-                //Let microphone start recording sound
-                recorder.startRecording();
-                //Instance for speaker
-                //AudioTrack atrack = new AudioTrack(AudioManager.STREAM_VOICE_CALL, Constants.Calling.SAMPLING_RATE, channelConfig, audioFormat, minBufSize, AudioTrack.MODE_STREAM);
-
-                AudioTrack atrack = new AudioTrack(AudioManager.STREAM_MUSIC, Constants.Calling.SAMPLING_RATE,
-                        AudioFormat.CHANNEL_OUT_MONO,
-                        AudioFormat.ENCODING_PCM_16BIT, minBufSize, AudioTrack.MODE_STREAM);
-
-                //set speaker sapling rate
-                atrack.setPlaybackRate(Constants.Calling.SAMPLING_RATE);
-                //start speaker
-                atrack.play();
+                //buffer to hold incoming sampled sound
+                byte[] receiveData = new byte[minBufSize];
                 //While call is running
                 while (mRunCallingServer) {
-                    //buffer to hold incoming sampled sound
-                    byte[] receiveData = new byte[minBufSize];
                     //new instance for datagram recived packet
-                    DatagramPacket receivePacket = new DatagramPacket(receiveData,
-                            receiveData.length);
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, minBufSize);
                     //receive data from server socket and hold it in receivePacket
                     serverSocket.receive(receivePacket);
                     Log.i(mTag, "Call server recived packet its size is: " + receivePacket.getData().length);
                     //write recived data to speaker
-                    atrack.write(receivePacket.getData(), 0, receivePacket.getLength());
-
-                    recorder.read(buffer, 0, buffer.length);
-                    //putting buffer in the packet
-                    DatagramPacket sendPacket = new DatagramPacket(buffer, buffer.length, receivePacket.getAddress(), receivePacket.getPort());
-                    //Send voice packet to destination
-                    serverSocket.send(sendPacket);
+                    atrack.write(receivePacket.getData(), 0, minBufSize);
                 }
                 serverSocket.disconnect();
                 serverSocket.close();
-                recorder.stop();
-                recorder.release();
                 atrack.stop();
                 atrack.flush();
                 atrack.release();
+                Log.w("Call server"," is closed");
             } catch (SocketException e) {
                 e.printStackTrace();
             } catch (UnknownHostException e) {
@@ -117,62 +89,38 @@ public class CallService extends Service {
         thread.start();
     }
 
-    private void startCallInstance(String serverIP) {
+    private void startMic(String serverIP) {
         Thread thread = new Thread(() -> {
+            //Instance for microphone
+            AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, Constants.Calling.SAMPLING_RATE,
+                    AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,
+                    AudioRecord.getMinBufferSize(Constants.Calling.SAMPLING_RATE,
+                            AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT) * 10);
             try {
-
+                y = 1;
                 //Address of destination call server
                 final InetAddress destination = InetAddress.getByName(serverIP);
                 //Servers to receive from client
                 DatagramSocket socket = new DatagramSocket();
-                //Instance for microphone
-                //AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,Constants.Calling.SAMPLING_RATE,channelConfig,audioFormat,minBufSize*10);
-
-                AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, Constants.Calling.SAMPLING_RATE,
-                        AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,
-                        AudioRecord.getMinBufferSize(Constants.Calling.SAMPLING_RATE,
-                                AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT) * 10);
-
                 //Let microphone start recording sound
                 recorder.startRecording();
-                //Instance for speaker
-                //AudioTrack atrack = new AudioTrack(AudioManager.STREAM_VOICE_CALL, Constants.Calling.SAMPLING_RATE, channelConfig, audioFormat, minBufSize, AudioTrack.MODE_STREAM);
-
-                AudioTrack atrack = new AudioTrack(AudioManager.STREAM_MUSIC, Constants.Calling.SAMPLING_RATE,
-                        AudioFormat.CHANNEL_OUT_MONO,
-                        AudioFormat.ENCODING_PCM_16BIT, minBufSize, AudioTrack.MODE_STREAM);
-
-                //set speaker sapling rate
-                atrack.setPlaybackRate(Constants.Calling.SAMPLING_RATE);
-                //start speaker
-                atrack.play();
+                //buffer to receive from microphone
+                byte[] buffer = new byte[minBufSize];
+                int bytes_read;
                 //While call is running
                 while (mRunCallingInstance) {
-                    //buffer to receive from microphone
-                    byte[] buffer = new byte[minBufSize];
-                    recorder.read(buffer, 0, buffer.length);
+                    bytes_read = recorder.read(buffer, 0, minBufSize);
                     //putting buffer in the packet
-                    DatagramPacket sendPacket = new DatagramPacket(buffer, buffer.length, destination, Constants.Calling.CALLING_SERVER_PORT);
+                    DatagramPacket sendPacket = new DatagramPacket(buffer, bytes_read, destination, Constants.Calling.CALLING_SERVER_PORT);
                     //Send voice packet to destination
                     socket.send(sendPacket);
                     Log.i(Constants.TAG, "call instance sent data its length is: " + sendPacket.getData().length);
-                    //buffer to hold incoming sampled sound
-                    byte[] receiveData = new byte[minBufSize];
-                    //new instance for datagram recived packet
-                    DatagramPacket receivePacket = new DatagramPacket(receiveData,
-                            receiveData.length);
-                    //receive data from server socket and hold it in receivePacket
-                    socket.receive(receivePacket);
-                    //write recived data to speaker
-                    atrack.write(receivePacket.getData(), 0, receivePacket.getLength());
                 }
                 socket.disconnect();
                 socket.close();
                 recorder.stop();
                 recorder.release();
-                atrack.stop();
-                atrack.flush();
-                atrack.release();
+                Log.w("Call Instance"," is closed");
             } catch (SocketException e) {
                 e.printStackTrace();
             } catch (UnknownHostException e) {
@@ -198,16 +146,17 @@ public class CallService extends Service {
             if (intent.getStringExtra("END") != null && intent.getStringExtra("END").equals("END")) {
                 mRunCallingInstance = false;
                 mRunCallingServer = false;
-                mDisposable.dispose();
+                x = 0;
+                y = 0;
                 return;
             }
-            if (ip == null) {
+            if (ip == null && x == 0) {
                 mRunCallingServer = true;
                 mRunCallingInstance = true;
-                startCallServer();
-            } else {
+                startSpeaker();
+            } else if (y == 0) {
                 mRunCallingInstance = true;
-                startCallInstance(ip);
+                startMic(ip);
             }
             //Todo Implement open call instance action
         }
